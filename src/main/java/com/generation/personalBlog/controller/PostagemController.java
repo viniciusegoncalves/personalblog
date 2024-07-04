b7709301784b60fe76283e4497b8b91ceea13076
Vinícius Gonçalves <viniciusebg25@icloud.com>
2024-07-04 13:09:00 -0300

    Put method

--- src/main/java/com/generation/personalBlog/controller/PostagemController.java
+++ src/main/java/com/generation/personalBlog/controller/PostagemController.java
@@ -48,6 +48,19 @@ public class PostagemController {
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+        return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+    }
+
+    @PutMapping
+    public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+        return postagemRepository.findById(postagem.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK)
+                .body(postagemRepository.save(postagem)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete (@PathVariable Long id) {
--- src/main/java/com/generation/personalBlog/repository/PostagemRepository.java
+++ src/main/java/com/generation/personalBlog/repository/PostagemRepository.java
@@ -2,8 +2,11 @@ package com.generation.personalBlog.repository;
 
 import com.generation.personalBlog.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface PostagemRepository extends JpaRepository<Postagem,Long> {
+import java.util.List;
 
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
-    }
+    public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+}
