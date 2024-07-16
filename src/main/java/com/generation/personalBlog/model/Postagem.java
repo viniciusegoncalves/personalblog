c5693fb4f736530ada52883a045e7f9f6ae452a7
Vinícius Gonçalves <viniciusebg25@icloud.com>
2024-07-16 20:57:26 -0300

    Implementando spring security.

--- src/main/java/com/generation/personalBlog/model/Postagem.java
+++ src/main/java/com/generation/personalBlog/model/Postagem.java
@@ -35,7 +35,10 @@ public class Postagem {
     private LocalDateTime data;
 
     @ManyToOne
-    @JsonIgnoreProperties("Postagem")
+    @JsonIgnoreProperties("postagem")
     private Tema tema;
 
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
 }
